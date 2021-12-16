c7a8ed13a5b2948ff43f5a52a5cf79a53ffdd104
hebertcisco <hebertcisco@outlook.com>
2021-12-16 11:56:25 -0300

    feat: :sparkles: add appcenter configuration

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+import * as React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import {ThemeProvider} from 'react-native-ios-kit';
+import App from './src/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+function Main() {
+  return (
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
