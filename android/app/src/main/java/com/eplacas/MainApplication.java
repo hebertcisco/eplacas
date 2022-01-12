29d0ef43938b4e8033d6164c38122b5913b48981
hebertcisco <hebertcisco@outlook.com>
2022-01-12 15:08:15 -0300

    refactor: :fire: remove error handle unused

--- android/app/src/main/java/com/eplacas/MainApplication.java
+++ android/app/src/main/java/com/eplacas/MainApplication.java
@@ -9,13 +9,12 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.google.firebase.analytics.FirebaseAnalytics;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
-    private FirebaseAnalytics mFirebaseAnalytics;
+
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
         @Override
@@ -47,7 +46,6 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-      mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
   }
 
   private static void initializeFlipper(
@@ -58,13 +56,7 @@ public class MainApplication extends Application implements ReactApplication {
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
+      } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
         e.printStackTrace();
       }
     }
