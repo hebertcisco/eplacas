e75d8450edc64b5a87d768b2964f7b92247c5fc0
hebertcisco <hebertcisco@outlook.com>
2022-03-27 17:03:22 -0300

    refactor: :construction: remove unused props

--- src/shared/hooks/useData.tsx
+++ src/shared/hooks/useData.tsx
@@ -1,8 +1,6 @@
 import React, { useCallback, useContext } from "react";
-import { AxiosResponse } from "axios";
 import { api } from "../services/api";
 import { IUseData } from "../interfaces/IUseData";
-import { ISearchResult } from "../interfaces/ISearchResult";
 import { Alert } from "react-native";
 import { storager } from "../services/storage";
 import { PLATES_CACHE_KEY } from "../constants/plates";
@@ -10,25 +8,23 @@ import { PLATES_CACHE_KEY } from "../constants/plates";
 export const DataContext = React.createContext({});
 
 export const DataProvider = ({ children }: { children: React.ReactNode }) => {
-  const handleSearchResult = useCallback((id: string) => {
+  const handleSearchResult = useCallback(async (id: string) => {
     const baseURL = `/consulta/${id}`;
-    return api
-      .get(baseURL)
-      .then(async (result: AxiosResponse<ISearchResult>) => {
-        const { data } = result;
-        await storager.set({
-          key: PLATES_CACHE_KEY,
-          id: data.id,
-          data: data
-        });
-        return data;
-      })
-      .catch((error) => {
-        console.error(error);
-        return Alert.alert("Erro", "Não foi possível realizar a busca.", [
-          { text: "OK" }
-        ]);
+    try {
+      const result = await api.get(baseURL);
+      const { data } = result;
+      await storager.set({
+        key: PLATES_CACHE_KEY,
+        id: data.id,
+        data: data
       });
+      return await data;
+    } catch (error) {
+      console.error(error);
+      return Alert.alert("Erro", "Não foi possível realizar a busca.", [
+        { text: "OK" }
+      ]);
+    }
   }, []);
 
   const contextValue = {
