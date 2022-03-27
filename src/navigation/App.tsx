2dc16c75199604cdb3b3a282d263a47feaf0f439
hebertcisco <hebertcisco@outlook.com>
2022-03-27 15:35:27 -0300

    feat: :construction: Added new history screen on routes

--- src/navigation/App.tsx
+++ src/navigation/App.tsx
@@ -3,25 +3,31 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/Home';
 import SearchResult from '../screens/SearchResult';
+import SearchHistory from '../screens/SearchHistory';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen
-                    options={{ headerShown: false }}
-                    name="Home"
-                    component={Home}
-                />
-                <Stack.Screen
-                    options={{ headerShown: true, title: 'Resultado' }}
-                    name="SearchResult"
-                    component={SearchResult}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Home"
+            component={Home}
+          />
+          <Stack.Screen
+            options={{ headerShown: true, title: "Resultado" }}
+            name="SearchResult"
+            component={SearchResult}
+          />
+          <Stack.Screen
+            options={{ headerShown: true, title: "Histórico de busca" }}
+            name="SearchHistory"
+            component={SearchHistory}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
 };
 
