1c611f133e6eb05153e3929dab61a26b0abafab4
hebertcisco <hebertcisco@outlook.com>
2022-03-27 15:32:48 -0300

    feat: :construction: saving the search content at the time of the request

--- src/shared/hooks/useData.tsx
+++ src/shared/hooks/useData.tsx
@@ -1,40 +1,43 @@
-import React, { useCallback, useContext } from 'react';
-import { AxiosResponse } from 'axios';
-import { api } from '../services/api';
-import { IUseData } from '../interfaces/IUseData';
-import { ISearchResult } from '../interfaces/ISearchResult';
-import { Alert } from 'react-native';
+import React, { useCallback, useContext } from "react";
+import { AxiosResponse } from "axios";
+import { api } from "../services/api";
+import { IUseData } from "../interfaces/IUseData";
+import { ISearchResult } from "../interfaces/ISearchResult";
+import { Alert } from "react-native";
+import { storager } from "../services/storage";
+import { PLATES_CACHE_KEY } from "../constants/plates";
 
 export const DataContext = React.createContext({});
 
 export const DataProvider = ({ children }: { children: React.ReactNode }) => {
-    const handleSearchResult = useCallback((id: string) => {
-        const baseURL = `/consulta/${id}`;
-        return api
-            .get(baseURL)
-            .then((result: AxiosResponse<ISearchResult>) => {
-                const { data } = result;
-                return data;
-            })
-            .catch((error) => {
-                console.error(error);
-                return Alert.alert(
-                    'Erro',
-                    'Não foi possível realizar a busca.',
-                    [{ text: 'OK' }]
-                );
-            });
-    }, []);
+  const handleSearchResult = useCallback((id: string) => {
+    const baseURL = `/consulta/${id}`;
+    return api
+      .get(baseURL)
+      .then(async (result: AxiosResponse<ISearchResult>) => {
+        const { data } = result;
+        await storager.set({
+          key: PLATES_CACHE_KEY,
+          id: data.id,
+          data: data
+        });
+        return data;
+      })
+      .catch((error) => {
+        console.error(error);
+        return Alert.alert("Erro", "Não foi possível realizar a busca.", [
+          { text: "OK" }
+        ]);
+      });
+  }, []);
 
-    const contextValue = {
-        handleSearchResult
-    };
+  const contextValue = {
+    handleSearchResult
+  };
 
-    return (
-        <DataContext.Provider value={contextValue}>
-            {children}
-        </DataContext.Provider>
-    );
+  return (
+    <DataContext.Provider value={contextValue}>{children}</DataContext.Provider>
+  );
 };
 
 export const useData = () => useContext(DataContext) as IUseData;
